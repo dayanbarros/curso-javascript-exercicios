5496b99d3300e8a07e832cc7381b4575ae078ea1
dayanbarros <dayan.r.barros@gmail.com>
2018-02-06 18:35:16 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arr = [1, 2, 3, 4, 5 ]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
