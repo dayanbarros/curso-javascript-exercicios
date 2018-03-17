15df44418e82eed9f73849c5f4f09878770ca8e7
dayanbarros <dayan.r.barros@gmail.com>
2018-03-17 11:48:46 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -54,7 +54,11 @@ showTeamPosition(6);
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var counter = 20;
+
+while(counter < 31){
+    console.log(counter);
+	counter++
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
